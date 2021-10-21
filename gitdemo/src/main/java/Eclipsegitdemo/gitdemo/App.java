427fb6cf858a4d04ceee258cbbb3cc36e945d2dc
SAHKUMA <SAHKUMA@CGSCLXR75678637.in623.corpintra.net>
2021-10-21 10:59:49 +0530

    pushing all code

---
+++ gitdemo/src/main/java/Eclipsegitdemo/gitdemo/App.java
@@ -0,0 +1,13 @@
+package Eclipsegitdemo.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
